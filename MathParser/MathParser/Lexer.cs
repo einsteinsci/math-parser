a3215e55c7486ef7dca9530a27801abfec9da59c
einsteinsci <einsteinsci@gmail.com>
2015-05-04 07:36:25 -0700

    Get this thing going

---
+++ MathParser/MathParser/Lexer.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using MathParser.Tokens;
+
+namespace MathParser
+{
+    public class Lexer
+    {
+		public static readonly Lexer Instance = new Lexer();
+
+		public string Expression
+		{ get; set; }
+
+		public List<Token> Tokens
+		{ get; private set; }
+    }
+}
