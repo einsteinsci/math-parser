722305a407a116dcb183db3b101fb747a7fa85ad
einsteinsci <einsteinsci@gmail.com>
2015-05-26 08:01:00 -0700

    Derp. Forgot to re-track the not operator

---
+++ MathParser/MathParser/ParseTree/NodeOperatorNot.cs
@@ -0,0 +1,31 @@
+﻿using MathParser.Tokens;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MathParser.ParseTree
+{
+	public class NodeOperatorNot : NodeOperatorUnary
+	{
+		public override string StringForm
+		{ get { return "~"; } }
+
+		public override Token Operator
+		{ get { return Token.OperatorNot; } }
+
+		public override MathType Type
+		{ get { return MathType.Boolean; } }
+
+		public NodeOperatorNot(NodeFactor term)
+		{
+			Term = term;
+		}
+
+		public override IResultValue GetResult()
+		{
+			return new ResultBoolean(!Term.GetResult().ToBoolean());
+		}
+	}
+}
